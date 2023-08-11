b61196980332f5b90b7ee43ba1102775bf3b9cff
João Paulo <paulocorrentinense@gmail.com>
2023-08-11 09:56:54 -0300

    v2

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
+const numeroSorteado = 8
+   
+const numeroDigitado = document.getElementById('numeroDigitado')
+numeroDigitado.focus()
+const buttonChutar = document.getElementById('chutar')
+
 const botaoSortear = document.getElementById('sortear')
-botaoSortear.addEventListener('click', () => {
-    const númeroSorteado = 8
-})
+// botaoSortear.addEventListener('click', () => {
 
+// })
 
-for(let c = 1 ;c < 4;c++) {
-
+function digitaNumero() {
+    numeroDigitado.focus()
+    numeroDigitado.value = ''
 }
+
+let numeroTentativas = 0
+buttonChutar.addEventListener('click', () => {
+
+
+    const numeroDigitadoNumber = Number(numeroDigitado.value)
+    if (numeroDigitadoNumber < 0 || numeroDigitadoNumber > 10) {
+        console.log("Valor inválido! Digite um número entre 0 e 10.")
+        numeroDigitado.value = ""
+        numeroDigitado.focus()
+    } else if (numeroTentativas < 2) {
+        if (numeroDigitadoNumber === numeroSorteado) {
+            console.log("Meus parabéns. Você acertou!!! O número era", + numeroSorteado)
+            numeroDigitado.value = ''
+           
+        } else {
+            console.log('ERROU')
+            digitaNumero()
+        }
+        
+        numeroTentativas++
+        console.log(numeroTentativas)
+    } else {
+        console.log('Você errou!!! O número era ' + numeroSorteado)
+        
+
+    }
+})
